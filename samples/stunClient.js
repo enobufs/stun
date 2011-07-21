c61fe4ac3b1b27551efcbf38e2add892d8db760f
Yutaka Takeda <ytakeda@Yutaka-Takeda.local>
2011-07-21 11:19:32 -0700

    First commit.

---
+++ samples/stunClient.js
@@ -0,0 +1,15 @@
+require.paths.unshift('../src');
+var stun = require('stun');
+
+//var STUN_SERVER_ADDR = "dntg-stun.usrd.scea.com";
+//var STUN_SERVER_ADDR = "stun1.noc.ams-ix.net";
+var STUN_SERVER_ADDR = "172.16.4.6";
+
+var client = stun.createClient();
+client.setServerAddr(STUN_SERVER_ADDR);
+client.start(function(result) {
+        var mapped = client.getMappedAddr();
+        console.log("Complete(" + result + "): " + (client.isNatted()?"Natted":"Open") + " NB=" + client.getNB() + " EF=" + client.getEF() + " (" + client.getNatType() + ") mapped=" + mapped.address + ":" + mapped.port + " rtt=" + client.getRtt());
+        client.close(function() {
+            console.log("All sockets closed.");});});
+
---
+++ samples/stunServer.js
@@ -0,0 +1,12 @@
+require.paths.unshift('../src');
+var stun = require('stun');
+
+// Load config file.
+var fs = require('fs');
+eval(fs.readFileSync('stunServer.conf', encoding="ascii"));
+
+var server = stun.createServer();
+server.setAddress0(settings.STUN_SERVER_ADDR_0);
+server.setAddress1(settings.STUN_SERVER_ADDR_1);
+server.listen();
+
